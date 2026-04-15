64b4303b437b7a4440c771ce5b63c71d311cb2d0
Nabil245 <13020240067@student.umi.ac.id>
2026-04-16 00:37:45 +0800

    Add files via upload
    hehe~

---
+++ Tugas3/MainMahasiswa.java
@@ -0,0 +1,20 @@
+package datamahasiswa;
+
+import java.util.Scanner;
+
+public class MainMahasiswa {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nama : ");
+        String nama = sc.nextLine();
+        System.out.print("NIM  : ");
+        String nim = sc.nextLine();
+        System.out.print("Jml MK: ");
+        int jmlMK = sc.nextInt();
+
+        Mahasiswa mhs = new Mahasiswa(nama, nim, jmlMK);
+        mhs.inputNilai(sc);
+        mhs.tampilRapor();
+    }
+}
